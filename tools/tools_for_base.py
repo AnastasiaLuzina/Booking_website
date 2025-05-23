ca701d5e6f77aafcbcddbc1ba8892450420d6872
Anastasia <nastayluzina777@gmail.com>
2025-05-24 01:54:41 +1000

    промежуточный перенос

---
+++ tools/tools_for_base.py
@@ -0,0 +1,26 @@
+
+import sqlite3
+
+DATABASE = 'booking_database.db'
+
+def connect_to_base():
+    try:
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        return cursor
+    except Exception as e:
+            return (print(e))
+
+def close_base():
+    try:
+        conn = sqlite3.connect(DATABASE)
+        conn.close()
+    except Exception as e:
+            return (print(e))
+
+def commit_in_base():
+    try:
+        conn = sqlite3.connect(DATABASE)
+        conn.commit()
+    except Exception as e:
+        return (print(e))
