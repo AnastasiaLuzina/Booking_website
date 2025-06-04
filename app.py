24d70b0fc96259806e333e0aa69f1a7875791b95
Anastasia <nastayluzina777@gmail.com>
2025-06-04 22:33:58 +1000

    правки

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from controllers.hallController import hall_bp
 from controllers.mainController import main_bp  
 from controllers.profileController import profile_bp  
 from controllers.equipmentController import equipment_bp
+from controllers.bookingController import booking_bp
+
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -13,6 +15,7 @@ app.register_blueprint(hall_bp)
 app.register_blueprint(main_bp)
 app.register_blueprint(profile_bp)
 app.register_blueprint(equipment_bp)
+app.register_blueprint(booking_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
