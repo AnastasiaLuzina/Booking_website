d417a96af39a610224ef695b000a2b65f86974fc
Anastasia <nastayluzina777@gmail.com>
2025-06-04 22:58:29 +1000

    фикс

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from controllers.mainController import main_bp
 from controllers.profileController import profile_bp  
 from controllers.equipmentController import equipment_bp
 from controllers.bookingController import booking_bp
-
+from controllers.photoController import photo_bp
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -16,6 +16,6 @@ app.register_blueprint(main_bp)
 app.register_blueprint(profile_bp)
 app.register_blueprint(equipment_bp)
 app.register_blueprint(booking_bp)
-
+app.register_blueprint(photo_bp)
 if __name__ == "__main__":
     app.run(debug=True)
